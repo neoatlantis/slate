8deef10e2dd3d34e57dee72ddd7afec3ff245fe5
NeoAtlantis <contact@chaobai.li>
2024-06-04 23:33:52 +0200

    aggregrate events

--- server-src/service/guardian/index.js
+++ server-src/service/guardian/index.js
@@ -1,3 +1,16 @@
 import "./usb_monitor";
 import "./pci_monitor";
 import "./login_monitor";
+
+import { channel } from "app/lib/endpoints";
+
+channel("service.guardian.login_monitor.changed").on(on_changed);
+channel("service.guardian.pci_monitor.changed").on(on_changed);
+channel("service.guardian.usb_monitor.changed").on(on_changed);
+
+const EVENT_NAME = "service.guardian.change_detected";
+channel(EVENT_NAME);
+
+function on_changed(){
+	channel(EVENT_NAME).trigger();
+}
