ef3a28a4cbd0df3dc0af8b63c57832addb979f23
NeoAtlantis <aurichalka@gmail.com>
2024-07-18 19:06:27 +0200

    add options page with endpoint url set/get

---
+++ web-extension/src/vue/options/index.vue
@@ -0,0 +1,56 @@
+<template><div>
+
+<table style="width:100%; padding: 0.5em" v-if="!loading">
+	<tr>
+		<td>Endpoint URL:</td>
+		<td>
+			<input v-model="endpoint_url" type="text" @change="changed=true" style="width:100%" />
+		</td>
+	</tr>
+
+	<tr>
+		<td colspan="2">
+			<button @click="save" :disabled="!changed">Save</button>
+		</td>
+	</tr>
+</table>
+<div v-else>
+	Loading...
+</div>
+
+</div></template>
+<script>
+import _ from "lodash";
+export default {
+	mounted(){
+		this.init();
+	},
+
+	data(){
+		return {
+			loading: true,
+			endpoint_url: "",
+			changed: false,
+		}
+	},
+
+	methods: {
+		async init(){
+			try{
+				let results = await browser.storage.sync.get("endpoint_url");
+				this.endpoint_url = _.get(results, "endpoint_url");
+			} finally {
+				this.loading = false;
+			}
+		},
+
+		async save(){
+			await browser.storage.sync.set({
+				endpoint_url: this.endpoint_url.toString(),
+			});
+			this.changed = false;
+		}
+	}
+
+}
+</script>
